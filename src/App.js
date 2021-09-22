58120aaf6281f8655b778e9d746ad8746df10f4c
JmsVh <ldg.velazquez@gmail.com>
2021-09-22 10:57:20 -0500

    Actualizacion titulo

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const fecha = new Date().getFullYear();
   return (
     <Fragment>
         <Header titulo="Titulo del sitio web"/>
-        <h2>Tienda de productos:</h2>
+        <h2>Productos:</h2>
             {productos.map(producto => (
                 <Producto
                     key={producto.id}
